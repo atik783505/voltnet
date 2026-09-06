c6e1b347ab3ddd411eaf975eebf658c5b2b1e6d2
atik783505 <atik13672@gmail.com>
2026-09-07 01:51:10 +0600

    mangestation section

---
+++ src/app/dashboard/company/stations/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageStatios = () => {
+    return (
+        <div>
+            <h2>Manage Stations</h2>
+        </div>
+    );
+};
+
+export default ManageStatios;
